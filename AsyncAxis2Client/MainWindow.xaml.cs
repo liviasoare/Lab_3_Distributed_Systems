5991a55a8ea7f6d45481f4d20c1d307d1a08bc47
Livia Soare <liv13soare@gmail.com>
2021-10-30 21:26:28 +0300

    4. Additional homework

--- AsyncAxis2Client/MainWindow.xaml.cs
+++ AsyncAxis2Client/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Timers;
 
 namespace AsyncAxis2Client
 {
@@ -19,39 +20,83 @@ namespace AsyncAxis2Client
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {  
+        private ServiceReference2.TextFileContentRetrieverPortTypeClient client;
+        private Timer GetContentTimer = new Timer(1000);
+        private int Seconds = 0;
+        
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public void GetResultAndUpdateLabels(IAsyncResult result)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                txtFileContent.Text = client.EndretrieveTextFileContent(result);
+                lblBlocked.Content = "";
+                GetContentTimer.Stop();
+            });
+        }
+
+        private void UpdateLabel(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                lblBlocked.Content = "Seconds elapsed: " + Seconds;
+                Seconds += 1;
+            });
+        }
+
         private void BtnGetContent_Click(object sender, RoutedEventArgs e)
         {
             string fileName = txtFileName.Text;
-            lblBlocked.Content = "Searching and unblocked";
+            //lblBlocked.Content = "Searching and unblocked";
             // lblBlocked.Content = "Searching and blocked";
-            txtFileContent.Text = "";
-            try
+            //txtFileContent.Text = "";
+
+
+            /*try
             {
                 ServiceReference1.TextFileContentRetrieverPortTypeClient client =
                     new ServiceReference1.TextFileContentRetrieverPortTypeClient();
                 client.retrieveTextFileContentCompleted += asyncMethodCompleted;
                 client.retrieveTextFileContentAsync(fileName);
 
-                /* Task<ServiceReference1.retrieveTextFileContentResponse> response = client.retrieveTextFileContentAsync(fileName);
-                response.ContinueWith((t) => asyncMethodCompleted(t)); */
-                // txtFileContent.Text = client.retrieveTextFileContent(fileName);
+                 Task<ServiceReference1.retrieveTextFileContentResponse> response = client.retrieveTextFileContentAsync(fileName);
+                response.ContinueWith((t) => asyncMethodCompleted(t)); 
+                 txtFileContent.Text = client.retrieveTextFileContent(fileName);
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 lblBlocked.Content = "";
             }
-            /*finally
+            finally
             {
                 lblBlocked.Content = "";
             } */
 
+            try
+            {
+                client = new
+                ServiceReference2.TextFileContentRetrieverPortTypeClient();
+                GetContentTimer.Elapsed += UpdateLabel;
+                GetContentTimer.Start();
+                IAsyncResult result = client.BeginretrieveTextFileContent(fileName, new AsyncCallback(GetResultAndUpdateLabels), null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                lblBlocked.Content = "";
+            }
+            finally
+            {
+                Seconds = 0;
+                lblBlocked.Content = "";
+            }
         }
 
         /* public void asyncMethodCompleted(Task<ServiceReference1.retrieveTextFileContentResponse> task)
@@ -61,11 +106,11 @@ namespace AsyncAxis2Client
             { txtFileContent.Text = result; lblBlocked.Content = ""; });
         } */
 
-            public void asyncMethodCompleted(object sender, ServiceReference1.retrieveTextFileContentCompletedEventArgs e)
+         /*   public void asyncMethodCompleted(object sender, ServiceReference1.retrieveTextFileContentCompletedEventArgs e)
         {
             this.Dispatcher.Invoke(() =>
             { txtFileContent.Text = e.Result; lblBlocked.Content = ""; });
-        }
+        } */
 
         private void BtnDoSomething_Click(object sender, RoutedEventArgs e)
         {
